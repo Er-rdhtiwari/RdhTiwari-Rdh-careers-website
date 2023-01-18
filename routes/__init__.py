6977f24955dc53e33f539f7f681d1104b3ca616e
rdhtiwari <radhe.shyam@pichainlabs.com>
2023-01-18 18:16:52 +0530

    about page setup

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,14 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
+import os
 app = Flask(__name__)
 
 
 @app.route("/")
 def helo_world():
-    return '<p> Hello, World! </p>'
+    print(os.getcwd())
+    return render_template('test/home.html')
+
+@app.route("/about")
+def about():
+    print(os.getcwd())
+    return render_template('test/about.html')
